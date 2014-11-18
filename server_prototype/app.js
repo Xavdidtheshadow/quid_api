b2622ddda682c7ec75f5c0cbe700bd5da1a7f745
David Brownman <beamneocube@gmail.com>
2014-11-17 21:58:16 -0800

    added a couple of routes

--- server_prototype/app.js
+++ server_prototype/app.js
@@ -9,12 +9,18 @@ app.get("/", function(request, response) {
     response.send("Hello world!");
 });
 
-app.get("/users", function(request, response) {
-    response.send("Users Query!");
-});
+// app.get("/users", function(request, response) {
+//     response.send("Users Query!");
+// });
+
+// could take comma separated lists to fetch multiple objects
 
 app.get("/users/:id", function(request, response) {
-    response.send({status: 200, query: request.params.id});
+    response.send({status: 200, results: {name: "David Brownman", team: request.params.id}});
+});
+
+app.get("/teams/:id", function(request, response) {
+    response.send({status: 200, results: {name: "Michigan Quidditch", id: request.params.id}});
 });
 
 // Start the app
