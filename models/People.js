b8015ec3a0c1cd17747668795e6c2b8653ebb40f
David Brownman <beamneocube@gmail.com>
2015-04-06 02:00:51 -0700

    teams to team

--- models/People.js
+++ models/People.js
@@ -25,7 +25,7 @@ var PersonSchema = new mongoose.Schema({
 
   // primary/current team is always first
   // this is no longer an array. that's silly.
-  teams: {type: {type: mongoose.Schema.Types.ObjectId, ref: "Team"}, default: null},
+  team: {type: {type: mongoose.Schema.Types.ObjectId, ref: "Team"}, default: null},
   // previous teams
   requests: {type: [{type: mongoose.Schema.Types.ObjectId, ref: "Team"}], default: []}
 });
