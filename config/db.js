598b8591fb219349703f4b9009aaa622178ed455
David Brownman <beamneocube@gmail.com>
2015-04-09 21:39:21 -0400

    url oops

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
-  url: "mongodb://localhost:27017/quidditch"
-  // url: process.env.MONGOLAB_URI
+  // url: "mongodb://localhost:27017/quidditch"
+  url: process.env.MONGOLAB_URI
 };
